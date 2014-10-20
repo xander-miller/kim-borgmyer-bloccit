6f3cb41b3925e975874d34ce2f2704981fd1f106
Kim Borgmeyer <Kim.Borgmeyer@gmail.com>
2014-10-19 17:22:06 -0700

    Created posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find( params[ :id ] )
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
+
+  resources :posts
+
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
+
 end
