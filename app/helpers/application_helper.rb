66c4da63d7942910ea1efa73f0e10bbdd8f46a53
Kim Borgmeyer <Kim.Borgmeyer@gmail.com>
2014-10-25 12:35:32 -0700

    Refactored markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,4 @@ module ApplicationHelper
 
   end
 
-  def markdown( text )
-
-    renderer = Redcarpet::Render::HTML.new
-    extensions = { fenced_code_blocks: true }
-    redcarpet = Redcarpet::Markdown.new( renderer, extensions )
-    ( redcarpet.render text ).html_safe
-    
-  end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,5 +10,24 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
-  
+
+  def markdown_title
+    render_as_markdown title
+  end
+
+  def markdown_body
+    render_as_markdown body
+  end
+
+  private
+
+  def render_as_markdown( text )
+
+    renderer = Redcarpet::Render::HTML.new
+    extensions = { fenced_code_blocks: true }
+    redcarpet = Redcarpet::Markdown.new( renderer, extensions )
+    ( redcarpet.render text ).html_safe
+    
+  end
+
 end
