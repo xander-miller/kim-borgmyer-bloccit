d5378ab5af720a3f6d743319d66141bb916c3545
Kim Borgmeyer <Kim.Borgmeyer@gmail.com>
2014-10-28 20:16:08 -0700

    Refactored policy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -5,7 +5,7 @@ class CommentPolicy < ApplicationPolicy
   end
 
   def destroy?
-    user.present? && ( record.user == user || user.admin? || user.moderator? )
+    user.present? && can_moderate?( user, record )
   end
 
 end
