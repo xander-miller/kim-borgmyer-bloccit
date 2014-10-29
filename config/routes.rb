2891157833e925158d6f09bb4dd623c2e3b6ab01
Kim Borgmeyer <Kim.Borgmeyer@gmail.com>
2014-10-28 19:29:18 -0700

    Implemented shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Bloccit::Application.routes.draw do
 
   get "comments/create"
+
   devise_for :users
   resources  :users, only: [ :update ]
 
   resources :topics do
-
-    resources :posts, except: [ :index ] do
-      resources :comments, only: [ :create, :destroy ]
-    end
-    
+    resources :posts, except: [ :index ]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [ :create, :destroy ]
+  end
+    
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
