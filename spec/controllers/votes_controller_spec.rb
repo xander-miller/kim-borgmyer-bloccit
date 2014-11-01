a6da117b72ab428763ec2a26ae349e040051fb50
Kim Borgmeyer <Kim.Borgmeyer@gmail.com>
2014-11-01 13:57:43 -0700

    Add controller spec for down_vote

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
- describe VotesController do
+describe VotesController do
 
-   include TestFactories
-   include Devise::TestHelpers
+  include TestFactories
+  include Devise::TestHelpers
 
-   describe '#up_vote' do
+  before do
+    @user = authenticated_user
+    @post = associated_post
+    sign_in @user
+    request.env[ "HTTP_REFERER" ] = '/'
+  end
+
+  describe '#up_vote' do
     it "adds an up-vote to the post" do
-
-      request.env[ "HTTP_REFERER" ] = '/'
-      
-      @user = authenticated_user
-      @post = associated_post
-      sign_in @user
 
       expect {
         post( :up_vote, post_id: @post.id )
       }.to change{ @post.up_votes }.by 1
     end
 
- end
-  
+  end
+
+  describe '#down_vote' do
+    it "adds a down-vote to the post" do
+
+      expect {
+        post( :down_vote, post_id: @post.id )
+      }.to change{ @post.down_votes }.by 1
+    end
+
+  end
+
 end
